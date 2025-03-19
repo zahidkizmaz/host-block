6becd929fe565991f1460dc3900ced624af5800c
Zahid Kizmaz <tech@zahid.rocks>
2025-03-19 21:21:11 +0100

    add pre-commit and dev flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Simple development shell flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pre-commit
+          ];
+        };
+      }
+    );
+}
